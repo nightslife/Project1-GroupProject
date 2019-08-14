d4dc72d1973dbc527678cd8b30a98d1e39ff1a5f
Paola Stone <paola.stonemartinez@dell.com>
2019-08-13 21:01:14 -0400

    fixed first page menu, and added search result display

--- assets/geocash.js
+++ assets/geocash.js
@@ -1,16 +1,24 @@
 
+//menu first page -open
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
-  
-  function closeNav() {
+ 
+//menu first page -close
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
   }
 
+// Field menu begin
+
+
+// Field menu end
+
+  
 var searchInput = "developer"
 var locationInput = ""
 var address = []
